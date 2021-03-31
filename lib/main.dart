83b0fc4f91cb65a50a7ad1fd41ae593998857676
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-03-31 14:59:29 +0900

    set: search input

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,8 +12,8 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: new ThemeData(
-        primaryColor: Colors.greenAccent,
-        scaffoldBackgroundColor: Colors.greenAccent[100],
+        primaryColor: Colors.orangeAccent[200],
+        scaffoldBackgroundColor: Colors.orangeAccent[180],
       ),
       home: new MyHomePage(),
     );
@@ -26,6 +28,49 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('入力フォーム', style: TextStyle(color: Colors.orange[800])),
+        centerTitle: true,
+      ),
+      body: Form(
+        child: Container(
+          padding: EdgeInsets.only(left: 30.0, right: 20.0),
+          child: Column(children: <Widget>[
+            Container(
+              child: Row(children: <Widget>[
+                Expanded(
+                    child: TextFormField(
+                  style: TextStyle(color: Colors.cyan[900]),
+                  maxLength: 20,
+                  maxLengthEnforced: true,
+                  obscureText: false,
+                  // autovalidate: false,
+                  decoration: InputDecoration(
+                      hintText: 'キーワードを入力してください',
+                      labelText: 'キーワードから探す',
+                      labelStyle: TextStyle(color: Colors.orange),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.brown),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.brown),
+                      )),
+                )),
+                IconButton(
+                  icon: Icon(
+                    Icons.search,
+                    size: 40.0,
+                  ),
+                )
+              ]),
+            ),
+            Container(),
+            Container(),
+            Container(),
+          ]),
+        ),
+      ),
+    );
   }
 }
